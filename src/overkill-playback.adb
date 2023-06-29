a8c2c2a3909b5de98bc9e44bafe82da7ab57f0de
Mateus de Lima Oliveira <mateus@ativarsoft.com>
2023-06-29 02:14:51 -0300

    Initial commit for playlist
    Signed-off-by: Mateus de Lima Oliveira <mateus@ativarsoft.com>

--- src/overkill-playback.adb
+++ src/overkill-playback.adb
@@ -1,8 +1,34 @@
+with Ada.Containers.Vectors;
+with Ada.Strings.Unbounded;
+use Ada.Strings.Unbounded;
+
 package body Overkill.Playback is
+
+   type Playlist_Element is record
+      Path: Unbounded_String;
+      Title: Unbounded_String;
+      Artist: Unbounded_String;
+   end record;
+
+   package Playlist_Vectors is new Ada.Containers.Vectors
+     (Element_Type => Playlist_Element,
+      Index_Type => Natural);
+      
+   type Playlist_Type is record
+      Entries : Playlist_Vectors.Vector;
+      Current_Position : Natural;
+   end record;
+
+   Playlist : Playlist_Type;
 
    procedure Previous is
    begin
-      null;
+      if Playlist.Current_Position > 0 then
+         Playlist.Current_Position := Playlist.Current_Position - 1;
+      else
+         Playlist.Current_Position := Integer (Playlist.Entries.Length);
+      end if;
+      Play;
    end Previous;
    
    procedure Play is
@@ -22,7 +48,13 @@ package body Overkill.Playback is
    
    procedure Next is
    begin
-      null;
+      if Playlist.Current_Position = Playlist.Current_Position then
+         Playlist.Current_Position := 0;
+      else
+         Playlist.Current_Position := Playlist.Current_Position + 1;
+         Play;
+      end if;
+      Play;
    end Next;
 
 end Overkill.Playback;
